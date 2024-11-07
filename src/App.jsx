b1d9e83c783b4d8ff67fd73910d4755228a5f5e0
RakeshkumarThera <rakeshkumarthera@gmail.com>
2024-11-07 13:29:01 -0500

    local storage and when to avoid using useEffect unnecessarily

--- src/App.jsx
+++ src/App.jsx
@@ -7,13 +7,15 @@ import DeleteConfirmation from './components/DeleteConfirmation.jsx';
 import logoImg from './assets/logo.png';
 import {sortPlacesByDistance} from './loc.js'
 
+const storedIds = JSON.parse(localStorage.getItem('selectedPlaces')) || [];
+const storedPlaces =  storedIds.map((id) => AVAILABLE_PLACES.find((place) => place.id === id));
 
 function App() {
   const modal = useRef();
   const selectedPlace = useRef();
   const [availablePlaces, setAvailablePlaces] = useState([]);
-  const [pickedPlaces, setPickedPlaces] = useState([]);
-
+  const [pickedPlaces, setPickedPlaces] = useState(storedPlaces);
+  
   useEffect(() => {                                            //UseEffect helps to execute after loading all components, 1sr arg is function and 2nd arg is dependencies, 2nd arg helps to decide how many times to reload the component
     navigator.geolocation.getCurrentPosition((position)=> {    //this is the side-effect
       const sortedPlaces = sortPlacesByDistance(
@@ -43,6 +45,11 @@ function App() {
       const place = AVAILABLE_PLACES.find((place) => place.id === id);
       return [place, ...prevPickedPlaces];
     });
+
+    const storedIds = JSON.parse(localStorage.getItem('selectedPlaces')) || [];
+    if(storedIds.indexOf(id) === -1){
+      localStorage.setItem('selectedPlaces', JSON.stringify([id, ...storedIds]));
+    }
   }
 
   function handleRemovePlace() {
@@ -50,6 +57,9 @@ function App() {
       prevPickedPlaces.filter((place) => place.id !== selectedPlace.current)
     );
     modal.current.close();
+
+    const storedIds = JSON.parse(localStorage.getItem('selectedPlaces')) || [];
+    localStorage.setItem('selectedPlaces', JSON.stringify(storedIds.filter((id) => id !== selectedPlace.current)));
   }
 
   return (
